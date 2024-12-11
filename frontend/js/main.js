7b3081184bf92b2770b514591a2c0aa1e9e722f0
Nikolas Opedal <nikolas.opedal@gmail.com>
2024-12-11 11:00:29 +0100

    f

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,6 +1,6 @@
 document.getElementById('generate-script-btn').addEventListener('click', function() {
     // Send a request to the server to generate the script
-    fetch('http://localhost:3000/generate-script')
+    fetch('https://ip-tracker-backend.vercel.app/api/generate-script')
         .then(response => response.json())
         .then(data => {
             // Display the generated script in the output container
